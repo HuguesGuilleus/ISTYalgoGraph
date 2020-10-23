c6ae1e7c8d05d7a88642526585c4033ae44f7571
hugues <ghugues@netc.fr>
2020-10-23 18:35:01 +0200

    rust/bin/graph.rs: simplification

--- rust/src/bin/graph.rs
+++ rust/src/bin/graph.rs
@@ -1,39 +1,21 @@
 #![feature(termination_trait_lib, process_exitcode_placeholder)]
 
-fn main() -> finalreturn::R {
-    let f = "../x/db/FacebookSites.csv";
-    // let f = "../x/db/GitHub.csv";
+fn main() -> Result<(), String> {
+    // Nombre de nœud au maximum dans le graphe.
+    let size: Option<usize> = Some(20_000);
+    // let size: Option<usize> = None;
+
+    // Fichier à charger.
+    // let f = "../x/db/FacebookSites.csv";
+    let f = "../x/db/GitHub.csv";
     // let f = "../x/db/RoadNetwork.txt";
     // let f = "../x/db/twitchDE.csv";
-    // let f = "../x/db/Wikipedia1.csv";()
-    // let f = "../x/db/Wikipedia2.csv";()
+    // let f = "../x/db/Wikipedia1.csv";
+    // let f = "../x/db/Wikipedia2.csv";
 
-    println!("Loading ...");
-    println!("{:#?}", graph::Graph::load(f, Some(10_000))?.stats());
+    let mut s = graph::Graph::load(f, size)?.stats();
+    s.degree_distrib = vec![];
+    println!("{:#?}", s);
 
     Ok(())
-}
-
-mod finalreturn {
-    pub type R = Result<(), FinalReturn>;
-
-    pub struct FinalReturn {
-        s: String,
-    }
-    impl std::convert::From<String> for FinalReturn {
-        fn from(s: String) -> Self {
-            FinalReturn { s: s }
-        }
-    }
-    impl std::fmt::Debug for FinalReturn {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-            f.write_str(&self.s)
-        }
-    }
-    impl std::process::Termination for FinalReturn {
-        fn report(self) -> i32 {
-            eprintln!("Error: {}", self.s);
-            return std::process::ExitCode::FAILURE.report();
-        }
-    }
 }
