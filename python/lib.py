1313d3cd1388fc02f2964bda916b3590b99c49d5
hugues <ghugues@netc.fr>
2020-10-26 19:45:26 +0100

    python/: Add Graph.__repr__()

--- python/lib.py
+++ python/lib.py
@@ -6,6 +6,49 @@ class Graph:
         for i in range(size or 0):
             self.adjacency_list.append([])
 
+    def __repr__(self):
+        """
+        >>> g = Graph(8);
+        >>> g.add((0, 1));
+        >>> g.add((0, 4));
+        >>> g.add((1, 6));
+        >>> g.add((3, 6));
+        >>> g.add((3, 2));
+        >>> g.add((5, 0));
+        >>> g.add((5, 1));
+        >>> g.add((5, 2));
+        >>> g.add((7, 6));
+        >>> g.add((7, 6));
+        >>> g
+        * 1 . . 1 . . .
+        . * . . . . 1 .
+        . . * . . . . .
+        . . 1 * . . 1 .
+        . . . . * . . .
+        1 1 1 . . * . .
+        . . . . . . * .
+        . . . . . . 2 *
+        """
+        s = ""
+        for line, child in enumerate(self.adjacency_list):
+            nodes = child.copy()
+            nodes.sort()
+            i = 0
+            for j in range(0, self.len()):
+                nb = 0
+                while i < len(nodes) and nodes[i] == j:
+                    nb += 1
+                    i += 1
+                if nb == 0:
+                    if j == line:
+                        s += "* "
+                    else:
+                        s += ". "
+                else:
+                    s += str(nb) + " "
+            s = s[:-1] + "\n"
+        return s[:-1]
+
     def add(self, arc):
         """Ajoute l'arc au graphe si le sommet de départ et d'arrivé sont dans le graphe."""
         begin = arc[0]
